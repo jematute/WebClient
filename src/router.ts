daf47ed10d27366f918857ca1888bd9d575f10db
Juan Matute <Juan.Matute@synergis.com>
2018-03-26 17:34:26 -0400

    WebClient

--- src/router.ts
+++ src/router.ts
@@ -2,20 +2,33 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
+import Login from './components/Login.vue';
+import VueRouter from 'vue-router';
 
 Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
-  ],
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About,
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+  },
+];
+
+const router = new VueRouter({ routes });
+
+router.beforeEach((to, from, next) => {
+  console.log(to);
 });
+
+export default router;
